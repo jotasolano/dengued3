7d87eb1262e0d606001ddbd4d849194b85587111
Antonio Solano <jsolano91@gmail.com>
2016-04-16 10:46:44 -0600

    fix(hover) and added scales

--- src/javascript/chart.js
+++ src/javascript/chart.js
@@ -1,13 +1,14 @@
 var d3 = require('d3');
+var $ = require('jquery');
 
 
 var variableWidthSTR = d3.select("#chart").style("width");
 var variableWidth = parseInt(variableWidthSTR.substring(0, variableWidthSTR.length - 2));
 
 // Heatmap
-var margin = { top: 50, right: 0, bottom: 100, left: 0 },
+var margin = { top: 50, right: 20, bottom: 100, left: 20 },
   width = variableWidth - margin.left - margin.right,
-  height = 430 - margin.top - margin.bottom,
+  height = 500 - margin.top - margin.bottom,
   gridSize = Math.floor(width / 52),
   legendElementWidth = gridSize*2,
   buckets = 9,
@@ -17,22 +18,38 @@ var margin = { top: 50, right: 0, bottom: 100, left: 0 },
 
 
 var weekScale = d3.scale.linear()
-  .domain([1, 53])
+  .domain([1, 52])
   .range([0,width])
 
 var regionScale = d3.scale.linear()
-  .domain([0, 10])
+  .domain([1, 9])
   .range([0,height])
 
 var casosScale = d3.scale.linear()
   .domain([0, 520])
   .range([0, colors.length])
 
+var xAxis = d3.svg.axis()
+    .scale(weekScale)
+    .orient("top")
+    .ticks(30)
+
+var yAxis = d3.svg.axis()
+    .scale(regionScale)
+    .orient("left")
+
 var svg = d3.select("#chart").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
+  .attr("class", "x axis")
+  .call(xAxis)
+  .attr("class", "y axis")
+  .call(yAxis)
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+var tooltip = d3.select(".tooltip")
 
 d3.json("data/heatmap.json", function(heatmap) {
 /* Add a LatLng object to each item in the dataset */
@@ -66,6 +83,7 @@ d3.json("data/heatmap.json", function(heatmap) {
       var currentVal = data[i].Regiones[t];
 
       svg.append("rect")
+        .attr("class", "rect" )
         .attr("x", currentX )
         .attr("y", function(d){ return regionScale(t) } )
         .attr("width", gridSize)
@@ -73,12 +91,42 @@ d3.json("data/heatmap.json", function(heatmap) {
         .attr("casos", currentVal.casos)
         .attr("region", currentVal.region)
         .attr("fill",function(){ return colors[ Math.round(casosScale(currentVal.casos)) ] })
-        .on("mouseover",function(d){
-          console.log(d)
-        })
     }
-
   }
+
+  function mouseoverRect(element,event) {
+    var casos = $(element).attr("casos");
+    var region = $(element).attr("region");
+    tooltip.transition()
+      .duration(200)
+      .style("opacity", .9)
+    tooltip.html("<p><strong>Casos: </strong>" + casos + "</p><br/>" +
+          "<p><strong>Region: </strong>" + region + "</p><br/>")
+      
+      .style("left", function(){
+                if (event.pageX > variableWidth /2) {
+                  return (event.pageX - 170) + "px";
+                }else{
+                  return (event.pageX) + "px";
+                }
+              })
+              .style("top", (event.pageY - 90) + "px")
+  }
+
+  function mouseoutRect(element) {
+    tooltip.transition()
+      .duration(200)
+      .style("opacity", 0)
+  }
+
+  $('.rect').hover(
+    function (e) {
+      mouseoverRect(this,e)
+    },
+    function () {
+      mouseoutRect(this)
+    }
+  )
 
 
 })
