6c3c7422d4feff5054bb6fd2293273aa285cb0c6
Antonio Solano <jsolano91@gmail.com>
2016-04-18 22:20:07 -0600

    beautify axes

--- src/javascript/chart.js
+++ src/javascript/chart.js
@@ -5,12 +5,10 @@ var $ = require('jquery');
 var variableWidthSTR = d3.select("#chart").style("width");
 var variableWidth = parseInt(variableWidthSTR.substring(0, variableWidthSTR.length - 2));
 
-// Heatmap
-var margin = { top: 30, right: 30, bottom: 30, left: 30 },
+var margin = { top: 30, right: 30, bottom: 30, left: 80 },
   width = variableWidth - margin.left - margin.right,
-  height = 450 - margin.top - margin.bottom,
+  height = 360 - margin.top - margin.bottom,
   gridSize = Math.floor(width / 52),
-  // legendElementWidth = gridSize*2,
   buckets = 9,
   colors = ['#9e0142','#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','#e6f598','#abdda4','#66c2a5','#3288bd','#5e4fa2']
   // colors = ['#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#a50f15','#67000d'], // alternatively colorbrewer.YlGnBu[9]
@@ -18,19 +16,21 @@ var margin = { top: 30, right: 30, bottom: 30, left: 30 },
   weeks = [];
 
 
+// Declare the scales
 var weekScale = d3.scale.linear()
   .domain([1, 52])
   .range([0,width])
 
 var regionScale = d3.scale.linear()
-  .domain([1, 8])
+  .domain([0, 8])
   .range([0,height])
-  // .range([height, 0])
 
 var casosScale = d3.scale.linear()
   .domain([0, 520])
   .range([0, colors.length])
 
+
+// Declare the axes
 var xAxis = d3.svg.axis()
     .scale(weekScale)
     .orient("top")
@@ -39,28 +39,44 @@ var xAxis = d3.svg.axis()
 var yAxis = d3.svg.axis()
     .scale(regionScale)
     .orient("left")
+    .tickFormat( function(index) {
+      var labels = ["Brunca", "Central E.", "Central N.", "Central S.", "Chorotega", "Huetar C.", "Huetar N.", "Occidente", "Pacífico C."];
+      return labels[index];
+    });
 
+// Declare the chart bounding box
 var svg = d3.select("#chart").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// Append the axes
 svg.append("g")
   .attr("class", "x axis")
-  // .attr("transform", "translate(0," + height + ")")
   .call(xAxis)
+  .selectAll("text")  
+    .style("text-anchor", "end")
+    .attr("dx", "1.1em")
+    .style("fill", "#4d4d4d")
 
 svg.append("g")
   .attr("class", "y axis")
-  // .attr("transform", "translate(" + width + ",0)")
   .call(yAxis)
+  .selectAll("text")  
+    .style("text-anchor", "end")
+    .attr("dy", "1.5em")
+    .style("fill", "#4d4d4d")
 
+// Declare the tooltop
 var tooltip = d3.select(".tooltip")
 
-d3.json("data/heatmap.json", function(heatmap) {
-/* Add a LatLng object to each item in the dataset */
 
+// Load the data
+d3.json("data/heatmap.json", function(heatmap) {
+
+
+// Format the data so that rect can consume it
 	heatmap.forEach(function(d) {
 		weeks.push(d.Semana);
 	});
@@ -94,7 +110,7 @@ d3.json("data/heatmap.json", function(heatmap) {
         .attr("x", currentX )
         .attr("y", function(d){ return regionScale(t) } )
         .attr("width", gridSize)
-        .attr("height", gridSize + 5)
+        .attr("height", gridSize + 8)
         .attr("casos", currentVal.casos)
         .attr("region", currentVal.region)
         .attr("fill",function(){ return colors[ Math.round(casosScale(currentVal.casos)) ] })
